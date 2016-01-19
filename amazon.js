b00c719d2e59772c096637cfc410da9cc879a8a4
CoderHam <hemanham@gmail.com>
2016-01-19 12:27:00 +0530

    Amazon Scraper (Beta) added

--- amazon.js
+++ amazon.js
@@ -3,17 +3,17 @@ var cheerio = require('cheerio');
 var express = require('express');
 var app = express();
 
-var url = 'www.flipkart.com';
-var s = '/search?q='
-var word ='Lenovo'
+var url = 'www.amazon.in';
+var s = '/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords='
+var word ='Lenovo' //producr name
 var comurl = url + s + word;
-var all = '.pu-details';
-var name = '.pu-title';
-var cat = '.pu-category';
-var price = '.pu-price';
-var pdetails = '.pu-usp';
+// var all = '.pu-details';
+var name = '.s-access-detail-page';
+//var cat = '.pu-category'; // a-text-bold inside a-size-small a-link-normal a-text-normal
+var price = '.s-price';
+// var pdetails = '.pu-usp';
 
-console.log('Scraping data from Flipkart URl: '+comurl);
+console.log('Scraping data from Amazon URl: '+comurl);
 
 var options = {
     host: url,
@@ -26,7 +26,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.get('/scrape', function (req, response) {
+app.get('/amazon_scrape', function (req, response) {
   var request = http.request(options, function (res) {
       var code = '';
       res.on('data', function (chunk) {
@@ -35,7 +35,7 @@ app.get('/scrape', function (req, response) {
       res.on('end', function () {
           var scraper = cheerio.load(code);
           var scraped = '';
-          scraper(name).filter(function() {
+          scraper(price).filter(function() { // select oee of name, price
             var data = scraper(this);
             console.log(data.text());
             scraped = scraped + data.text()+';';
